c81c8ff8cf7c1259ce629ff9ceb687e4cc5f475d
ujjwalpuri29 <ujjwalpuri29@yahoo.com>
2021-07-12 22:55:27 +0530

    Edit Largest Numbers in Arrays.js

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting Challenges/Largest Numbers in Arrays.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting Challenges/Largest Numbers in Arrays.js
@@ -17,4 +17,4 @@ function largestOfFour(arr) {
     return result;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
