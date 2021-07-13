026a307fee621228f5821782c4cad8a2e2cc257f
ujjwalpuri29 <ujjwalpuri29@yahoo.com>
2021-07-13 23:47:31 +0530

    Add Boo who.js

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting Challenges/Boo who.js
@@ -0,0 +1,12 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+    if (bool === true || bool === false)
+        return true;
+    return false;
+}
+  
+booWho(null);
